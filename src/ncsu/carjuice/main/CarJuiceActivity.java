67f534765fd83d07d019f7836e232d32599f8d97
Dan Perjar <dperjar@ncsu.edu>
2012-04-23 11:37:38 -0400

    Got a textbox and submit button to display text in another activity view

--- src/ncsu/carjuice/main/CarJuiceActivity.java
+++ src/ncsu/carjuice/main/CarJuiceActivity.java
@@ -1,14 +1,30 @@
 package ncsu.carjuice.main;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 //Test comment
 public class CarJuiceActivity extends Activity {
+	
+	public final static String EXTRA_MESSAGE = "com.example.myapp.MESSAGE";
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        //another comment
+    }
+    
+    /** Called when the user selects the Send button */
+    public void sendMessage(View view) {
+        // Do something in response to button
+    	Intent intent = new Intent(this, SearchableActivity.class);
+    	EditText editText = (EditText) findViewById(R.id.edit_message);
+    	String message = editText.getText().toString();
+    	intent.putExtra(EXTRA_MESSAGE, message);
+    	//Starts instance of the activity called by intent parameter, in this case: DisplayMessageActivity
+    	startActivity(intent);
     }
 }

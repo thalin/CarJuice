610d59a489a2987d0942b42de52dd7b4c196dcc4
Dan Perjar <dperjar@ncsu.edu>
2012-04-25 02:04:10 -0400

    Had the search view forward to the list as well, which now has a few more items. Didn't get to any location stuff.
    Current bug: Search passes
    intent to SearchableActivity which then immediately creates an intent
    which it uses to forward to the ListView(ResultsActivity). Using the
    back button from the ListView goes to the blank SearchableActivity
    layout though. Should probably go straight back to the main screen but
    we'll roll with it for demonstrations sake.

--- src/ncsu/carjuice/main/CarJuiceActivity.java
+++ src/ncsu/carjuice/main/CarJuiceActivity.java
@@ -8,24 +8,34 @@ import android.widget.EditText;
 //Test comment
 public class CarJuiceActivity extends Activity {
 	
-	public final static String EXTRA_MESSAGE = "ncsu.carjuice.main.MESSAGE";
+	//Can add things like strings to an intent and send those along too
+	//public final static String EXTRA_MESSAGE = "ncsu.carjuice.main.MESSAGE";
 
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        //Open the search dialogue on app-launch
+        onSearchRequested();
     }
     
-
+    
+    //Message sent by "Locate" button. Sends an intent to the listview activity
     public void sendMessage(View view) {
+    	
         // Do something in response to button
     	Intent intent = new Intent(this, ResultsListActivity.class);
-    	/*
+    	
+    	/*This is code for grabbing the text out of a text box on the main page,
+    	 * not currently in use but in case we need to go back to that route...
+    	 * 
     	EditText editText = (EditText) findViewById(R.id.edit_message);
     	String message = editText.getText().toString();
     	intent.putExtra(EXTRA_MESSAGE, message);
     	*/
+    	
     	//Starts instance of the activity called by intent parameter, in this case: DisplayMessageActivity
     	startActivity(intent);
     }

3474cef683d5a1cf0ef27dab251f4fc87ccd04a8
dperjar <dperjar@ncsu.edu>
2012-04-25 22:58:00 -0300

    Update gen/ncsu/carjuice/main/R.java

--- gen/ncsu/carjuice/main/R.java
+++ gen/ncsu/carjuice/main/R.java
@@ -11,7 +11,15 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int car_icon=0x7f020000;
+        public static final int gear=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int locate_down=0x7f020003;
+        public static final int locate_up=0x7f020004;
+        public static final int main_background=0x7f020005;
+        public static final int main_layout=0x7f020006;
+        public static final int mainscreen_car_juice=0x7f020007;
+        public static final int mainscreen_plug=0x7f020008;
     }
     public static final class id {
         public static final int textView1=0x7f060000;
